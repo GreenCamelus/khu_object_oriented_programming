596f65893f2059cce9f53db5774187975f1c7e09
Sungdae Kim <73771530+GreenCamelus@users.noreply.github.com>
2023-11-13 16:00:08 +0900

    Create main.cpp

---
+++ ClassExercise/Ex2/Ex3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "store.h"
+using namespace std;
+
+int main() {
+	store A, B;
+	B = store(3, 4, 5, 7, 100, 200, 300, 500);
+	cout << A.total() << endl;
+	cout << B.total() << endl;
+
+	B *= 3;
+
+	cout << B.total() << endl;
+}
